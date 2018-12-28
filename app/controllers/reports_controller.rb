b1409b16910de26243b4c9d2b4bfca97f0bc67ad
lokideos <archmage09@gmail.com>
2018-12-28 17:30:58 +0300

    Use memorization instead of before_action for assigining @report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :load_report, only: %i[show edit update destroy]
-
   def index
     @reports = Report.all
   end
 
-  def new
-    @report = Report.new
-  end
+  def new; end
 
   def show; end
 
@@ -18,8 +14,8 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
 
-    if @report.save
-      redirect_to @report, notice: 'Report has been successfully created.'
+    if report.save
+      redirect_to report, notice: 'Report has been successfully created.'
     else
       flash[:notice] = 'There were some errors in your input.'
       render :new
@@ -27,8 +23,8 @@ class ReportsController < ApplicationController
   end
 
   def update
-    if @report.update(report_params)
-      redirect_to @report, notice: 'Report has been successfully updated.'
+    if report.update(report_params)
+      redirect_to report, notice: 'Report has been successfully updated.'
     else
       flash[:notice] = 'There were some errors in your input.'
       render :edit
@@ -36,16 +32,18 @@ class ReportsController < ApplicationController
   end
 
   def destroy
-    @report.destroy
+    report.destroy
 
     redirect_to root_path, notice: 'Report has been successfully deleted'
   end
 
   private
 
-  def load_report
-    @report = Report.find(params[:id])
+  def report
+    @report ||= params[:id] ? Report.find(params[:id]) : Report.new
   end
+
+  helper_method :report
 
   def report_params
     params.require(:report).permit(:title, :description)
