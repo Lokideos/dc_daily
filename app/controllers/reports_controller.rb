62f0c49f9aa1923a806a41e80e0b15a9d72dd16d
lokideos <archmage09@gmail.com>
2018-12-30 21:53:45 +0300

    Localize reports notifications

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,18 +15,18 @@ class ReportsController < ApplicationController
     @report = Report.new(report_params)
 
     if report.save
-      redirect_to report, notice: 'Report has been successfully created.'
+      redirect_to report, notice: t('reports.notification.created')
     else
-      flash[:notice] = 'There were some errors in your input.'
+      flash[:notice] = t('common.invalid_attributes_error')
       render :new
     end
   end
 
   def update
     if report.update(report_params)
-      redirect_to report, notice: 'Report has been successfully updated.'
+      redirect_to report, notice: t('reports.notification.updated')
     else
-      flash[:notice] = 'There were some errors in your input.'
+      flash[:notice] = t('common.invalid_attributes_error')
       render :edit
     end
   end
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
   def destroy
     report.destroy
 
-    redirect_to root_path, notice: 'Report has been successfully deleted'
+    redirect_to root_path, notice: t('reports.notification.deleted')
   end
 
   private
